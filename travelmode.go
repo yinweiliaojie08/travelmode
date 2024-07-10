7148d0df93f3f2400c8b55405ae235857a39ccbe
yinweiliaojie08 <67277453+yinweiliaojie08@users.noreply.github.com>
2024-07-10 14:11:53 +0800

    Update travelmode.go

--- travelmode.go
+++ travelmode.go
@@ -5,14 +5,14 @@ import (
 	"fmt"
 )
 
-func Priceaircraft(cost interface{}) {
-	fmt.Printf("飞机飞的很快，价格有点高，本次花费%d \n", cost)
+func Aircraft(place string, cost interface{}) {
+	fmt.Printf("已到达目的：%s,飞机飞的很快，价格有点高，本次花费%d元。 \n", place, cost)
 }
 
-func Pricecycling(cost interface{}) {
-	fmt.Printf("骑行有点慢，路上买了点水就行，本次花费%d \n", cost)
+func Cycling(place string, cost interface{}) {
+	fmt.Printf("已到达目的：%s,骑行有点慢，路上买了点水就行，本次花费%d元。 \n", place, cost)
 }
 
-func Pricetrain(cost interface{}) {
-	fmt.Printf("火车性价比很高，没花多少钱，本次花费%d \n", cost)
+func Train(place string, cost interface{}) {
+	fmt.Printf("已到达目的：%s,火车性价比很高，没花多少钱，本次花费%d元。 \n", place, cost)
 }
