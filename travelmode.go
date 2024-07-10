61ef7bb0e5e6c8a3a4750201cd578573590a95db
yinweiliaojie08 <18736506195@163.com>
2024-07-10 14:26:50 +0800

    .

--- travelmode.go
+++ travelmode.go
@@ -6,13 +6,13 @@ import (
 )
 
 func Aircraft(place string, cost interface{}) {
-	fmt.Printf("已到达目的：%s,飞机飞的很快，价格有点高，本次花费%d元。 \n", place, cost)
+	fmt.Printf("已到达目的：%s,飞机飞的很快，价格有点高，本次花费%d元。\n", place, cost)
 }
 
 func Cycling(place string, cost interface{}) {
-	fmt.Printf("已到达目的：%s,骑行有点慢，路上买了点水就行，本次花费%d元。 \n", place, cost)
+	fmt.Printf("已到达目的：%s,骑行有点慢，路上买了点水就行，本次花费%d元。\n", place, cost)
 }
 
 func Train(place string, cost interface{}) {
-	fmt.Printf("已到达目的：%s,火车性价比很高，没花多少钱，本次花费%d元。 \n", place, cost)
+	fmt.Printf("已到达目的：%s,火车性价比很高，没花多少钱，本次花费%d元。\n", place, cost)
 }
